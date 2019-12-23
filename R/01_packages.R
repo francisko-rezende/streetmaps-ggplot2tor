3e50fe2aa5b70153e3babdbf9cceec803b023ad4
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2019-12-23 16:14:12 +0200

    added gitignore, rproj and script for loading/installing packs

---
+++ R/01_packages.R
@@ -0,0 +1,15 @@
+# checking for and installing needed packages -----------------------------
+
+# code from Shane's reply to this stack overflow answer:
+# https://stackoverflow.com/questions/4090169/elegant-way-to-check-for-missing-packages-and-install-them
+
+packages <- c("osmdata", "tidyverse")
+
+new.packages <- packages[!(packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+
+
+# loading packages --------------------------------------------------------
+
+library(osmdata)
+library(tidyverse)
